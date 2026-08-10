d96305957b581be5b92a41cc069e3ea3a5e0ac9a
Shakeel_Rehman <shakeelrehman501@gmail.com>
2026-08-11 02:49:50 +0500

    simple express deployment

--- backend/api/index.js
+++
@@ -1,7 +0,0 @@
-import app from "../src/app.js";
-import connectDB from "../src/database/db.js";
-
-export default async function handler(req, res) {
-  await connectDB();
-  return app(req, res);
-}
