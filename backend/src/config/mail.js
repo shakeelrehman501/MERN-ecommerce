c35bdcf232566061d6ba66bd053f45040e0dcd45
Shakeel_Rehman <shakeelrehman501@gmail.com>
2026-08-12 15:46:20 +0500

    gmail transporter

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -2,12 +2,24 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
 });
+
+
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
 
 // Verify transporter connection
 transporter.verify((error, success) => {
